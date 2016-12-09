d69ecc9e770a35fe1922f947165f62c3615c06d1
Juan Rodriguez <juan.j.rodriguez2@gmail.com>
2016-12-08 21:55:18 -0600

    added file structure

---
+++ data/app.js
@@ -0,0 +1,24 @@
+	function config($routeProvider){
+		$routeProvider
+		    .when('/home',{
+		       templateUrl : "data/partials/home.html",
+		       controller : "MainController"
+		    })
+		    // .when('/blog',{
+		    //    templateUrl : "data/partials/BlogNav.html"
+		    // })
+		    //  .when('/admin',{
+		    //   templateUrl : "data/partials/admin/admin.login.html"
+		    // })
+		    // .when('/adminConsole/',{
+		    //   templateUrl : "data/partials/admin/admin.console.html"
+		    // })
+		    .otherwise({
+		      redirectTo: '/home'
+		    });
+	}
+
+
+angular
+	.module('app',['ngRoute','firebase'])
+	.config(config);
